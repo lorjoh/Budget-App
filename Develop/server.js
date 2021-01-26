5f7d6b4469d51aa5964f6d046be8df1589e04378
SAMIA <68030713+lorjoh@users.noreply.github.com>
2021-01-26 17:06:08 -0500

    deposits

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Financial_Depth_db",
+  process.env.MONGODB_URI || "mongodb://localhost/deposits",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
